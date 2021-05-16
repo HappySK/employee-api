b660170196ff506bb327c71b1f566c48badd7494
HappySK <shravanyoserene1729@gmail.com>
2021-05-16 20:59:34 +0530

    Necessary routes has been configure

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express()
+import employeeRouter from './routes/employee.js'
 
-dotenv.config()
-app.use(express.json())
-app.use(cors())
+const app = express();
 
-const { DB_URL, PORT } = process.env
-mongoose.connect(DB_URL, {useNewUrlParser : true, useUnifiedTopology : true}, () => {
-  app.listen(PORT, () => {
-    console.log(`Server Started on Port : ${PORT}`)
-  })
-})
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+app.use('/employee', employeeRouter)
+
+const { DB_URL, PORT } = process.env;
+mongoose.connect(
+	DB_URL,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	(err) => {
+		if (!err) {
+			app.listen(PORT, () => {
+				console.log(`Server Started on Port : ${PORT}`);
+			});
+		} else console.log(err.message);
+	}
+);
