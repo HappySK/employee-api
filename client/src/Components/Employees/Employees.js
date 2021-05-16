06fbaac251f863caeff8efb16e0a9da046a85278
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 01:11:11 +0530

    Employee component

---
+++ client/src/Components/Employees/Employees.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Employee from './Employee/Employee'
+
+const Employees = () => {
+  return (
+    <div>
+      <h1>Employees</h1>
+      <Employee/>
+    </div>
+  )
+}
+
+export default Employees
