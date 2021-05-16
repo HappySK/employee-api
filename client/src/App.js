195510bac51049498b96545bfadc03cddf77600c
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 01:09:33 +0530

    Framed Layout and included components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Grid, Container, AppBar, Toolbar, Typography } from "@material-ui/core";
+import { useDispatch } from 'react-redux'
+
+import Employees from "./Components/Employees/Employees";
+import Form from "./Components/Form/Form";
+import { getAllEmployees } from './actions/employees'
 
 const App = () => {
-  return (
-    <div>
-      <h1>App Component</h1>
-    </div>
-  )
-}
 
-export default App
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllEmployees())
+  })
+  
+	return (
+		<div>
+        <Container>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6">Employee Details</Typography>
+            </Toolbar>
+          </AppBar>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item xs={12} lg={6}>
+              <Employees />
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+		</div>
+	);
+};
+
+export default App;
