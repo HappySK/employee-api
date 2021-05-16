d76b5be783541907711ac7dd2b9cb0e943754eaf
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 01:13:35 +0530

    Styles file for Form component

---
+++ client/src/Components/Form/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles({
+  form : {
+    padding: '20px'
+  },
+  textField : {
+    padding: '10px',
+    width: '90%',
+    align: 'center'
+  },
+  paper: {
+    backgroundColor : 'sky'
+  }
+})
