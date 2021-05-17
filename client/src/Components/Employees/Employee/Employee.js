1acef3c81055c55d372b55baf7d16bcd1b3ea2a3
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 19:51:40 +0530

    Added Update employee logic

--- client/src/Components/Employees/Employee/Employee.js
+++ client/src/Components/Employees/Employee/Employee.js
@@ -1,21 +1,49 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { Button, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@material-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { updateEmployee } from '../../../actions/employees'
 
 const Employee = () => {
 
   const emp = useSelector(state => state.employeeReducer)
+  const dispatch = useDispatch()
+
+  const handleEmployeeDetails = (employee) => {
+    dispatch(updateEmployee(employee))
+    console.log(employee)
+  }
 
   return (
     <div>
-      {
-        emp.length && emp.map((employee, index) => 
-          <div key={index}>
-            <h2>Employee Name : {employee.emp_name}</h2>
-            <h6>Employee Email : {employee.emp_email}</h6>
-            <h6>Employee Phone Number : {employee.emp_phone_no}</h6>
-            <h6>Employee Salary : {employee.emp_salary}</h6>
-          </div>)
-      }
+        <TableContainer component={Paper} elevation={3}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Phone Number</TableCell>
+                <TableCell>Salary</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                (emp.length !== 0) && emp.map(({_id, emp_name, emp_email, emp_phone_no, emp_salary}, index) => 
+                    <TableRow key={index}>
+                      <TableCell >{emp_name}</TableCell>
+                      <TableCell >{emp_email}</TableCell>
+                      <TableCell>{emp_phone_no}</TableCell>
+                      <TableCell>{emp_salary}</TableCell>
+                      <TableCell>
+                        <Button variant="outlined" color="secondary" onClick={() => handleEmployeeDetails({_id, emp_name, emp_email, emp_phone_no, emp_salary})}>Edit Employee</Button>
+                      </TableCell>
+                    </TableRow>
+                )
+              }
+            </TableBody>
+          </Table>
+        </TableContainer>
     </div>
   )
 }
