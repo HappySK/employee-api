b65b9b5800c2d40ca3b04ad27cf8bad241ff07b7
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 16:12:56 +0530

    Managing form data has been done

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -1,70 +1,75 @@
 import React, { useState } from "react";
-import { Paper, Button, TextField } from "@material-ui/core";
+import { Paper, Button, TextField, Typography } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
 import makeStyles from "./styles";
+import { createEmployee } from "../../actions/employees";
 
 const Form = () => {
 	const classes = makeStyles();
 
-  const initialState = {
-    emp_name : '',
-    emp_email : '',
-    emp_phone_no : '',
-    emp_salary : ''
-  }
-  const [employeeData, setEmployeeData] = useState(initialState) 
+	const dispatch = useDispatch();
+
+	const initialState = {
+		emp_name: "",
+		emp_email: "",
+		emp_phone_no: "",
+		emp_salary: "",
+	};
+	const [employeeData, setEmployeeData] = useState(initialState);
 
 	const handleChange = (e) => {
-    setEmployeeData({...employeeData, [e.target.name] : e.target.value})
-  }
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  };
+		setEmployeeData({ ...employeeData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch(createEmployee(employeeData));
+		console.log(employeeData);
+	};
 
 	return (
 		<div>
 			<form onSubmit={handleSubmit} className={classes.form} method="post">
 				<Paper elevation={3} className={classes.paper}>
+					<Typography variant="h5" align="center">
+						Employee Data Submission
+					</Typography>
 					<TextField
-            name="emp_name"
+						name="emp_name"
 						variant="outlined"
 						label="Employee Name"
 						className={classes.textField}
 						size="small"
-            value={employeeData.emp_name}
-            onChange={handleChange}
-						placeholder="Enter Employee Name"
+						value={employeeData.emp_name}
+						onChange={handleChange}
 					/>
 					<TextField
-            name="emp_email"
+						name="emp_email"
 						variant="outlined"
 						label="Employee Email"
 						className={classes.textField}
 						size="small"
-            value={employeeData.emp_email}
-            onChange={handleChange}
-						placeholder="Enter Employee Email"
+						value={employeeData.emp_email}
+						onChange={handleChange}
 					/>
 					<TextField
-            name="emp_phone_no"
+						name="emp_phone_no"
 						variant="outlined"
 						label="Employee Phone Number"
 						className={classes.textField}
 						size="small"
-            value={employeeData.emp_phone_no}
-            onChange={handleChange}
-						placeholder="Enter Employee Phone Number"
+						value={employeeData.emp_phone_no}
+						onChange={handleChange}
 					/>
 					<TextField
-            name="emp_salary"
+						name="emp_salary"
 						variant="outlined"
 						label="Employee Salary"
 						className={classes.textField}
 						size="small"
-            value={employeeData.emp_salary}
-            onChange={handleChange}
-						placeholder="Enter Employee Salary"
+						value={employeeData.emp_salary}
+						onChange={handleChange}
 					/>
 					<Button type="submit" variant="contained" color="primary" fullWidth>
 						Submit
