9e51c23209a5c5177fbd13eb39de6a3f0677638d
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 16:12:08 +0530

    Included the API call for creating an employee

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 const URL = 'http://localhost:5000/employee'
 
 export const getEmployees = () => axios.get(URL)
+export const createEmployee = (employeeData) => axios.post(`${URL}/create`, employeeData)
