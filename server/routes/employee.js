2231fae838d3409e0e4e16488ea0c7f2d754124f
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 19:53:32 +0530

    updateEmployee router logic

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { getEmployees, createEmployee } from '../controllers/employee.js'
+import { getEmployees, createEmployee, updateEmployee } from '../controllers/employee.js'
 
 const route = express.Router()
 
 route.get('/', getEmployees)
 route.post('/create', createEmployee)
+route.patch('/update/:id', updateEmployee)
 
 export default route
