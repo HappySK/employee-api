fa4a49a084074ed5ca58b088d1209ad0b3e6ca63
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 19:52:49 +0530

    Fixed mongoose deprecation issue

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use('/employee', employeeRouter)
 
 const { DB_URL, PORT } = process.env;
+mongoose.set('useFindAndModify', false)
 mongoose.connect(
 	DB_URL,
 	{ useNewUrlParser: true, useUnifiedTopology: true },
@@ -25,3 +26,4 @@ mongoose.connect(
 		} else console.log(err.message);
 	}
 );
+
