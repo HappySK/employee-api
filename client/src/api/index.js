40acb1caf9704fadd09f903c0e3886b2734ca0f9
HappySK <shravanyoserene1729@gmail.com>
2021-05-17 19:51:00 +0530

    Included updateEmployee API call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,4 @@ const URL = 'http://localhost:5000/employee'
 
 export const getEmployees = () => axios.get(URL)
 export const createEmployee = (employeeData) => axios.post(`${URL}/create`, employeeData)
+export const updateEmployee = (employee) => axios.patch(`${URL}/update/${employee._id}`, employee)
